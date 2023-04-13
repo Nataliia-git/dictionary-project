ef50fe7b676083963b7e8340a62f2abd0c71e669
Nataliia-git <119956364+Nataliia-git@users.noreply.github.com>
2023-04-13 23:41:39 +0200

    Integrated the images rendering

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyWord, setKeyWord] = useState(props.defaultKeyWord);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -16,10 +18,19 @@ export default function Dictionary(props) {
     let apiKey = "4a0a9aa45ad0959e9357bf0of17t2779";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let imagesApiKey = "4a0a9aa45ad0959e9357bf0of17t2779";
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyWord}&key=${imagesApiKey}`;
+    axios.get(imagesApiUrl).then(processImages);
   }
   function handleResponse(response) {
     setResults(response.data);
   }
+
+  function processImages(response) {
+    setPhotos(response.data.photos);
+  }
+
   function updateKeyWord(event) {
     setKeyWord(event.target.value);
   }
@@ -45,6 +56,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
