e2106ae14dd08e8becda4f52013fd19fd5fcf486
Nataliia-git <119956364+Nataliia-git@users.noreply.github.com>
2023-04-13 23:50:19 +0200

    Added the alt description for photos

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
             return (
               <div className="col-md-4" key={index}>
                 <a href={photo.url} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.alt}
+                  />
                 </a>
               </div>
             );
